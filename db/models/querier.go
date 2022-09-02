5340feb0bca95fc1247c88ffe8a37b082f810a0a
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2022-09-02 15:48:08 +0900

    add user models

--- db/models/querier.go
+++ db/models/querier.go
@@ -16,6 +16,8 @@ type Querier interface {
 	CreateTransfer(ctx context.Context, arg CreateTransferParams) (Transfer, error)
 	GetTransfer(ctx context.Context, id int64) (Transfer, error)
 	ListTransfers(ctx context.Context, arg ListTransfersParams) ([]Transfer, error)
+	CreateUser(ctx context.Context, arg CreateUserParams) (User, error)
+	GetUser(ctx context.Context, username string) (User, error)
 }
 
 var _ Querier = (*Queries)(nil)
